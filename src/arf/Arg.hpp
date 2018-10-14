59bec491e0659967bdea93741eff7949635e46b6
g <gtmalmgren@gmail.com>
2018-10-13 23:17:29 -0700

    Added Doxygen documnetation, and logo!

--- src/arf/Arg.hpp
+++ src/arf/Arg.hpp
@@ -5,25 +5,69 @@
 #include <vector>
 
 namespace arf {
+    /**
+     * @brief An abstract command line argument.
+     *
+     * Actual command line arguments must be implemented as a FuncArg or RefArg.
+     */
     class Arg {
     public:
+        /**
+         * @brief Long names for the Argument
+         *
+         * These are the long names for the argument. Long names will be prefixed with `--`.
+         */
         std::vector<std::string> names;
 
+        /**
+         * @brief Short (single character) aliases for the Argument
+         *
+         * These are the short (single character) aliases for the argument. Aliases are prefixed
+         * with a single leading `-`, and may be stacked until an argument requires a value.
+         */
         std::vector<std::string> aliases;
 
         explicit Arg(std::string name);
 
+        /**
+         * @brief Parses the Arg value, raises an Exception if the stream is not consumed.
+         *
+         * This calls the Arg::parse_hook method, and ensures that the stream was consumed.
+         *
+         * @param stream stream to be parsed
+         */
         void parse(std::istringstream& stream);
 
+        /**
+         * @brief Add an alias to the Arg.
+         *
+         * Add an alias (single character) to the Arg, this can be used as a short alias.
+         *
+         * @param alias
+         */
         void add_alias(std::string alias);
 
+        /**
+         * @brief Add multiple aliases to the Arg in a sginle call.
+         *
+         * @tparam Alias resolves to std::string
+         * @param alias a std::string alias or name (single or multi-character)
+         * @param aliases pass as a parameter pack
+         */
         template<typename... Alias>
         void add_alias(std::string alias, Alias... aliases) {
             this->add_alias(alias);
             this->add_alias(aliases...);
         };
 
-    protected:
-        virtual void _parse(std::istream& stream) = 0;
+        /**
+         * @brief Parse the Arg
+         *
+         * Must be overridden in subclass. A user of `argreffunc` can write their own subclass for
+         * special parsing.
+         *
+         * @param stream a stream for parsing
+         */
+        virtual void parse_hook(std::istream& stream) = 0;
     };
 }
--- src/arf/FuncArg.hpp
+++ src/arf/FuncArg.hpp
@@ -5,17 +5,39 @@
 #include <functional>
 
 namespace arf {
+    /**
+     * @brief An Arg that passes the command line value to a user-specified function.
+     *
+     * A FuncArg is created through the Parser::add or Parser::add_positional methods.
+     *
+     * @tparam T argument type of the user-specified function
+     */
     template<typename T>
     class FuncArg : public Arg {
     private:
         std::function<void(T)> func;
 
     public:
+        /**
+         * @brief Create a new FuncArg with a name and a function.
+         *
+         * @param name name of the argument
+         * @param func function to be called with the argument value
+         */
         FuncArg(const std::string name, std::function<void(T)> func)
           : Arg(name)
           , func(func){};
 
-        void _parse(std::istream& stream) override {
+        /**
+         * @brief Default parse implementation
+         *
+         * This is a pretty naive parse, just `>>` to a temporary variable, and then call the
+         * function. Error handling exists elsewhere to ensure that the entire stream has been
+         * consumed.
+         *
+         * @param stream stream to be parsed.
+         */
+        void parse_hook(std::istream& stream) override {
             T value;
             stream >> value;
             this->func(value);
